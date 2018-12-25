86c8d6be87b6411b3642f05fdfdcde2562b90a8c
pqueiros <pqueiros@gmail.com>
2018-12-25 14:33:48 +0100

    fix error "descriptor 'split' requires a 'str' object but received a 'unicode'"
    fix error "descriptor 'split' requires a 'str' object but received a 'unicode'"

--- maxcube/cube.py
+++ maxcube/cube.py
@@ -73,7 +73,7 @@ class MaxCube(MaxDevice):
 
     def parse_response(self, response):
 
-        lines = str.split(response, '\n')
+        lines = response.split('\n')
 
         for line in lines:
             line = line.strip()
